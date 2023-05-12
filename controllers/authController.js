1ca3fa89eead3b33bc05d7c324d3857d9c3b939e
Marwan8766 <marwanyasser344@gmail.com>
2023-05-12 22:58:29 +0200

    12/5

--- controllers/authController.js
+++ controllers/authController.js
@@ -460,8 +460,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     return next(new AppError('This user does no longer exist'), 401);
 
   // check if user confirmed his email
-  if (!currentUser.emailConfirmed)
-    return next(new AppError('You must confirm your email first', 403));
+  // if (!currentUser.emailConfirmed)
+  //   return next(new AppError('You must confirm your email first', 403));
 
   // check if the user has chamged his password after the token was issued
   if (
