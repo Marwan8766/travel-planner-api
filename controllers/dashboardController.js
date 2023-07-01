e26c2ba8124b3cfd6d66758d806cf8f711400e91
Marwan8766 <marwanyasser344@gmail.com>
2023-07-01 06:05:24 +0200

    getMostUsedService

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -601,6 +601,7 @@ exports.getMostSellingProducts = catchAsync(async (req, res, next) => {
 
   // Execute the find operation to get the most selling products
   const bookings = await Booking.find(matchCondition)
+    .limit(4)
     .select('quantity price tour tripProgram')
     .populate({
       path: 'tour',
