7087832a6f21ac8c1c7d52c55cee136bbd7d7de3
Marwan8766 <marwanyasser344@gmail.com>
2023-07-03 05:28:22 +0200

    getMostUsedService

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -21,16 +21,6 @@ exports.addToCart = catchAsync(async (req, res, next) => {
   if (!updatedCart)
     return next(new AppError('Error adding this item to the cart', 400));
 
-  // update item availablity
-  // const availability = await Availability.findOne({
-  //   tour: cartItem.tour,
-  //   tripProgram: cartItem.tripProgram,
-  //   date: cartItem.date,
-  // });
-
-  // availability.availableSeats = availability.availableSeats - cartItem.quantity;
-  // await availability.save({ validateModifiedOnly: true });
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -45,19 +35,6 @@ exports.createCartItems = catchAsync(async (req, res, next) => {
   const { cartItems } = req.body;
   const cart = await Cart.create({ user: req.user._id, items: cartItems });
   if (!cart) return next(new AppError('Error creating cart', 400));
-
-  // cartItems.forEach(async (cartItem) => {
-  // update item availablity
-  //   const availability = await Availability.findOne({
-  //     tour: cartItem.tour,
-  //     tripProgram: cartItem.tripProgram,
-  //     date: cartItem.date,
-  //   });
-
-  //   availability.availableSeats =
-  //     availability.availableSeats - cartItem.quantity;
-  //   await availability.save({ validateModifiedOnly: true });
-  // });
 
   res.status(200).json({
     status: 'success',
