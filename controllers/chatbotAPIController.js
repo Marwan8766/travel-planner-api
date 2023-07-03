1cf4200c4600221c79558e1652b99e4207f02392
Marwan8766 <marwanyasser344@gmail.com>
2023-07-03 21:31:19 +0200

    first commit2

--- controllers/chatbotAPIController.js
+++ controllers/chatbotAPIController.js
@@ -147,7 +147,7 @@ exports.chatbotWebhookHandler = catchAsync(async (req, res, next) => {
       );
 
       res.status(200).json({
-        richContent: [[...fulfillmentMessagesList]],
+        payload: { richContent: [[...fulfillmentMessagesList]] },
       });
 
       return;
