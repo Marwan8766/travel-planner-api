80d5a5ea809b05969625f006a5087720273e8af6
Marwan8766 <marwanyasser344@gmail.com>
2023-07-03 04:08:28 +0200

    getMostUsedService

--- models/review.model.js
+++ models/review.model.js
@@ -201,8 +201,6 @@ reviewSchema.pre('find', function (next) {
     select: '-_id name',
     options: { lean: true }, // Add this line if you want the populated user to be a plain JavaScript object
   });
-  this._mongooseOptions.populate = { virtuals: false }; // Exclude virtuals from population
-
   this._mongooseOptions.transform = (doc, ret) => {
     if (ret.user && ret.user.name) {
       ret.user = ret.user.name;
